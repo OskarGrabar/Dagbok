f79e9b9c21f4e09ad17fb92e76b1c2485ebe2ce1
OskarGrabar <gros24ul@student.ju.se>
2024-12-20 09:41:32 +0100

    Lägger till en springboot projekt

---
+++ dagbok/src/test/java/com/example/dagbok/DagbokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dagbok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagbokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
